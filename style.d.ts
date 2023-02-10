f75ff31cc01b1862aabcc137912c36f246ca289d
lrenc <haojie.lrenc@gmail.com>
2023-02-10 16:41:31 +0800

    区分样式和资源类文件定义

--- style.d.ts
+++ style.d.ts
@@ -1,21 +1,13 @@
-declare module "*.png" {
-  const value: any;
-  export = value;
-}
-
-declare module "*.jpg" {
-  const value: any;
-  export = value;
-}
-
 declare module '*.module.css' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
 declare module '*.module.scss' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
 declare module '*.module.sass' {
   const classes: { readonly [key: string]: string };
   export default classes;
